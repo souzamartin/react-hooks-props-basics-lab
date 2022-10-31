ced1a6c1faceebd317a4edc05aee4fc8b6ad76c2
Martin <souzam@sonic.net>
2022-10-31 13:15:02 -0400

    commit tree 994104035957d7ec016ddef5e403df516b2e23ee

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,27 @@
 import React from "react";
 
-function About() {
+function Links(props) {
+  return (
+    <>
+      <h3>Links</h3>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}>{props.linkedin}</a>
+    </>
+  )
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {props.bio ? <p>{props.bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links
+        github={props.github}
+        linkedin={props.linkedin}
+      />
     </div>
   );
 }
 
-export default About;
+export default About
